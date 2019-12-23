55688268e27b5666f45e41334e0dc54ce85d84ae
Zac Eskridge <zace118@gmail.com>
2019-12-23 14:17:46 -0700

    Got the local storage working with help from my tutor. Built a bad ass for each loop for the persisting.

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
-$("#Save").on('click', function(){
-    let userInput = $('#inputInfo').val();
-    localStorage.setItem('9AM', userInput);
-    console.log(localStorage);
+$(".save").on('click', function(){
+    console.log($(this).siblings('textarea').attr('id'));
+    let userInput = $(this).siblings('textarea').val();
+    let userHour = $(this).siblings('textarea').attr('id');
+    localStorage.setItem(userHour, userInput);
 });
+
+$('textarea').each(function(){
+    $('#'+ $(this).attr('id')).val(localStorage.getItem($(this).attr('id')));
+})
