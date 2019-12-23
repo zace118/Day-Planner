346eb7c4a9c9edde6713de6e4070f0db642fbbcf
Zac Eskridge <zace118@gmail.com>
2019-12-23 15:49:28 -0700

    Set up some more style, added notes in script, tried starting the moment functionality.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// This function sets variables that grab the sibling textarea of the save button, grab it's value and ID name and puts them as the key and value to local storage
 $(".save").on('click', function(){
     console.log($(this).siblings('textarea').attr('id'));
     let userInput = $(this).siblings('textarea').val();
@@ -5,6 +6,22 @@ $(".save").on('click', function(){
     localStorage.setItem(userHour, userInput);
 });
 
+// This function looks crazy, but it concatenates the id to call it, and grabs the value of the corresponding area from localStorage. Woo!
 $('textarea').each(function(){
     $('#'+ $(this).attr('id')).val(localStorage.getItem($(this).attr('id')));
 })
+
+
+
+// Setting variable for current time trying to figure out the color corridinating of the past, present and future time slots.
+
+let presentTime = moment().format('HH:mm');
+console.log(presentTime);
+
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
+    
+// }
+
+// let pastTime = i < presentTime;
+// let futureTime = i > presentTime;
