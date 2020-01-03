4a58c0db02aad04290da246aca1fbecb2b3abec3
Zac Eskridge <zace118@gmail.com>
2020-01-02 19:30:25 -0700

    Checking for the save

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 // This function sets variables that grab the sibling textarea of the save button, grab it's value and ID name and puts them as the key and value to local storage
 $(".save").on('click', function(){
-    console.log($(this).siblings('textarea').attr('id'));
+    console.log($(this).siblings('textarea').val());
     let userInput = $(this).siblings('textarea').val();
     let userHour = $(this).siblings('textarea').attr('id');
     localStorage.setItem(userHour, userInput);
 });
 
-// This function looks crazy, but it concatenates the id to call it, and grabs the value of the corresponding area from localStorage. Woo!
+// This function concatenates the id for the call and grabs the value of the corresponding area from localStorage.
 $('textarea').each(function(){
+    // console.log($('#'+ $(this).attr('id')));
+    // console.log($(localStorage.getItem($(this).attr('id'))));
     $('#'+ $(this).attr('id')).val(localStorage.getItem($(this).attr('id')));
-})
-
-
+}); 
 
 // Setting variable for current time trying to figure out the color corridinating of the past, present and future time slots.
 
-let presentTime = moment().format('HH:mm');
-console.log(presentTime);
+// let presentTime = moment().format('HH:mm');
+// console.log(presentTime);
 
 // for (let i = 0; i < array.length; i++) {
 //     const element = array[i];
